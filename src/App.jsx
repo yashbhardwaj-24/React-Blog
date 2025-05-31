f3578b2613321f3b008ac2925382c18808918cb3
yashbhardwaj24 <yash.yyy.24@gmail.com>
2025-05-31 08:02:41 +0530

    Export and Import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 // import Card from "./Components/Card"
 // import Day1 from "./Components/Day1"
-
-import Day2 from "./Components/Day2"
+// import Day2 from "./Components/Day2"
+import UserProfile, { Key, Login, Setting } from "./Components/UserProfile"
 
 function App(){
   return (
-    <Day2/>
+    <div>
+      <UserProfile/>
+      <Login/>
+      <Setting/>
+      <h1>{Key}</h1>
+    </div>
+    
   )
   
 }
---
+++ src/Components/UserProfile.jsx
@@ -0,0 +1,20 @@
+function UserProfile(){
+    return(
+        <h1>User Profile</h1>
+        
+    )
+}
+export default UserProfile  // default export
+
+export function Login(){  // named export
+    return (
+        <h1>Login</h1>
+    )
+}
+
+export function Setting(){
+    return(
+        <h1>Setting</h1>
+    )
+}
+export const Key = "dfdsdsdccacczc";
